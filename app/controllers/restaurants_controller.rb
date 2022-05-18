1622614318419c297542c1d766e35993c8396ee2
Augusto Leone <augustoleone04@gmail.com>
2022-05-18 16:02:24 +0200

    done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,14 +1,44 @@
 class RestaurantsController < ApplicationController
-  def name
-
+  def index
+    @restaurants = Restaurant.all
   end
 
-  def address
-
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
-  def category
-    # ["chinese", "italian", "japanese", "french", "belgian"]
+  def new
+    @restaurant = Restaurant.new
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to restaurants_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.delete
+    redirect_to restaurants_path
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
 end
